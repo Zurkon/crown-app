a55e085cb0a7d38fe1ffe5e33b8edb625adb6b9e
Zurkon <lukeyoshioka@gmail.com>
2020-12-09 23:41:03 -0300

    Update menu-items navigation withRouter

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -11,27 +11,32 @@ class Directory extends React.Component {
       sections: [{
         title: 'hats',
         imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+        linkUrl: 'hats',
         id: 1
       },
       {
         title: 'jackets',
         imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+        linkUrl: 'jackets',
         id: 2
       },
       {
         title: 'sneakers',
         imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+        linkUrl: 'sneakers',
         id: 3
       },
       {
         title: 'women',
         imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+        linkUrl: 'women',
         size: 'large',
         id: 4
       },
       {
         title: 'men',
         imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+        linkUrl: 'men',
         size: 'large',
         id: 5
       }]
@@ -42,8 +47,8 @@ class Directory extends React.Component {
     return (
       <div className="directory-menu">
         {
-          this.state.sections.map(({ id, title, imageUrl, size }) => {
-            return <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+          this.state.sections.map(({ id, title, imageUrl, size, linkUrl }) => {
+            return <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} linkUrl={linkUrl} />
           })
         }
       </div>
