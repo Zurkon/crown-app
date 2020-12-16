a81f84939c31a6eb49401df9aa67233d1a3531e5
Zurkon <lukeyoshioka@gmail.com>
2020-12-16 20:03:43 -0300

    Updated CheckoutPage component and added CheckoutItem component

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -4,6 +4,8 @@ import { createStructuredSelector } from 'reselect';
 
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 
+import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+
 import './checkout.styles.css';
 
 const CheckoutPage = ({ cartItems, total }) => (
@@ -28,7 +30,7 @@ const CheckoutPage = ({ cartItems, total }) => (
 
     {
       cartItems.map(cartItem => (
-        cartItem.name
+        <CheckoutItem key={cartItem.id} cartItem={cartItem} />
       ))
     }
 
